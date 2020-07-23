8b99138d1b2b0e3348a2173b6d2a7a5b92e332b0
Selenophilia <pauloargenal@gmail.com>
2020-07-23 12:35:32 +0800

    Initial Commit

--- Javascript/Lesson7/homework.js
+++ Javascript/Lesson7/homework.js
@@ -1,50 +1,65 @@
 // Do not change any of the function names
 
-function createUserClass(username, name, email, password) {
-  // create an ES6 class or a constructor function called User
-  // it should accept an options object with username, name, email, and password properties
-  // in the constructor set the username, name, email, and password properties
-  // the constructor should have a method 'sayHi' on its prototype that returns the string
-  // 'Hello, my name is {{name}}'
-  // {{name}} should be the name set on each instance
-  // return the class
-
-  this.username = username;
-  this.name     = name;
-  this.email    = email;
-  this.password = password;
-
+function invokeCallback(cb) {
+  // invoke cb
+  return cb();
 }
 
-createUserClass.prototype.sayHi = function(){
-    return ` 'Hello, my name is ${name}`
+const displayStr = () => 'hello' 
+// const invokeCallback = (cb) => cb();
+
+function sumArray(numbers, cb) {
+  // sum up all of the integers in the numbers array
+  // pass the result to cb
+  // no return is necessary
+  return cb(numbers)
 }
 
-function addPrototypeMethod(Constructor) {
-  // add a method to the constructor's prototype
-  // the method should be called 'sayHi' and should return the string 'Hello World!'
+// const sumArray = (numbers, cb) => cb(numbers);
+
+const  summation = (arr) => {
+  let temp = 0;
+  arr.forEach( elem => {
+      temp+=elem;
+  })
+  return temp;  
 }
 
-function addReverseString(str) {
-  // add a method to the string constructor's prototype that returns a reversed copy of the string
-  // name this method reverse
-  // hint:
-  // you will need to use 'this' inside of reverse
-  this.str = str;
+
+function forEach(arr, cb) {
+  // iterate over arr and pass its values to cb one by one
+  // hint: you will be invoking cb multiple times (once for each value in the array)
+  arr.forEach( elem => {
+      cb(elem)
+  })
 }
 
-addReverseString.prototype.reverse = function(){
-  let len = this.str.length
-  for(var i = len; i >= 0; i--){
-      console.log(str[i])
-  }
+// const myForEach = (arr) => arr.forEach( elem => { cb(elem) }) 
+
+const displayArr = (val) => val;
+
+function map(arr, cb) {
+  // create a new array
+  // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
+  // the new array should be the same length as the array argument
+
+  return cb(arr)
+}
+
+// const map = (arr, cb) => cb(arr)
+
+const getNewArr = (arr) => {
+  return newArr = arr.map( elem => {
+      return elem;
+  }) 
 }
 
 // Do not modify code below this line.
 // --------------------------------
 
 module.exports = {
-  createUserClass,
-  addPrototypeMethod,
-  addReverseString,
+  invokeCallback,
+  sumArray,
+  forEach,
+  map,
 };
